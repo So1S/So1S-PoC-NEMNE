fb66df130bb2317b2d72634920c1f69fdd7813fe
NEM-NE <dlatqdlatq@mju.ac.kr>
2022-06-21 03:05:14 +0900

    add: 로컬 api서버로 인퍼런스 시동 * 기존에 설정된 인그레스 기반으로 동작

--- api/src/main/java/com/example/api/model/application/ModelService.java
+++ api/src/main/java/com/example/api/model/application/ModelService.java
@@ -1,16 +1,43 @@
 package com.example.api.model.application;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import io.fabric8.kubernetes.api.model.apps.Deployment;
+import io.fabric8.kubernetes.client.DefaultKubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import lombok.AllArgsConstructor;
+
 @Service
+@AllArgsConstructor
 public class ModelService {
+	private final Logger logger = LoggerFactory.getLogger(ModelService.class);
+	private final KubernetesClient client = new DefaultKubernetesClient();
 
-	public boolean upload(){
-		/*
+	public String upload(){
 
-		 */
+		String result = "";
+		try {
+			io.fabric8.kubernetes.api.model.Service aService = client.services().load(new FileInputStream("/Users/imseongbin/Desktop/workspace/so1s/so1s-poc-nemne/inferences-server/kubernetes-manifests/inference.service.yaml")).get();
+			logger.info("service spec: {}", aService.getSpec());
+			io.fabric8.kubernetes.api.model.Service createdSvc = client.services().inNamespace("default").createOrReplace(aService);
+			Deployment aDeployment = client.apps()
+				.deployments()
+				.load(new FileInputStream(
+					"/Users/imseongbin/Desktop/workspace/so1s/so1s-poc-nemne/inferences-server/kubernetes-manifests/inference.deployment.yaml"))
+				.get();
+			result = aDeployment.toString();
+			logger.info("deployment spec: {}", aDeployment.getSpec());
+			Deployment createdDeployment = client.apps().deployments().inNamespace("default").createOrReplace(aDeployment);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-
-		return true;
+		return result;
 	}
 }
--- api/src/main/java/com/example/api/model/presentation/ModelController.java
+++ api/src/main/java/com/example/api/model/presentation/ModelController.java
@@ -1,24 +1,35 @@
 package com.example.api.model.presentation;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.api.model.application.ModelService;
 
-@RestController("/model")
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/model")
+@AllArgsConstructor
 public class ModelController {
+	private final Logger logger = LoggerFactory.getLogger(ModelService.class);
 	private ModelService modelService;
 
 	@PostMapping("/upload")
-	public ResponseEntity<Boolean> upload(){
-		/*
+	public ResponseEntity<String> upload(){
+		logger.debug("CONTROLLER!!");
+		String result = modelService.upload();
 
-		일단 모델 업로드를 요청한다. -> 모델을 매개변수 값으로 준다.
+		return ResponseEntity.ok(result);
+	}
 
-		 */
-
-		return ResponseEntity.ok(true);
+	@GetMapping
+	public ResponseEntity<String> hello(){
+		return ResponseEntity.ok("Hello World!");
 	}
 
 }
