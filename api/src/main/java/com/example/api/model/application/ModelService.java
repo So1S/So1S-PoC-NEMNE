df2aaa9956287467e56fd15964c0688951fb52c0
NEM-NE <dlatqdlatq@mju.ac.kr>
2022-06-22 00:29:41 +0900

    add: kubenetes에서 구축한 api서버로 인퍼런스 서버 시동

--- api/src/main/java/com/example/api/model/application/ModelService.java
+++ api/src/main/java/com/example/api/model/application/ModelService.java
@@ -1,14 +1,20 @@
 package com.example.api.model.application;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import io.fabric8.kubernetes.api.model.IntOrString;
+import io.fabric8.kubernetes.api.model.Quantity;
+import io.fabric8.kubernetes.api.model.ResourceRequirements;
+import io.fabric8.kubernetes.api.model.ResourceRequirementsBuilder;
+import io.fabric8.kubernetes.api.model.ServiceBuilder;
 import io.fabric8.kubernetes.api.model.apps.Deployment;
+import io.fabric8.kubernetes.api.model.apps.DeploymentBuilder;
 import io.fabric8.kubernetes.client.DefaultKubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import lombok.AllArgsConstructor;
@@ -20,24 +26,62 @@ public class ModelService {
 	private final KubernetesClient client = new DefaultKubernetesClient();
 
 	public String upload(){
+		io.fabric8.kubernetes.api.model.Service service = new ServiceBuilder()
+			.withNewMetadata()
+			.withName("inference-service")
+			.endMetadata()
+			.withNewSpec()
+			.withSelector(Collections.singletonMap("app", "myinference"))
+			.addNewPort()
+			.withPort(5000)
+			.withTargetPort(new IntOrString(5000))
+			.endPort()
+			.endSpec().build();
 
-		String result = "";
-		try {
-			io.fabric8.kubernetes.api.model.Service aService = client.services().load(new FileInputStream("/Users/imseongbin/Desktop/workspace/so1s/so1s-poc-nemne/inferences-server/kubernetes-manifests/inference.service.yaml")).get();
-			logger.info("service spec: {}", aService.getSpec());
-			io.fabric8.kubernetes.api.model.Service createdSvc = client.services().inNamespace("default").createOrReplace(aService);
-			Deployment aDeployment = client.apps()
-				.deployments()
-				.load(new FileInputStream(
-					"/Users/imseongbin/Desktop/workspace/so1s/so1s-poc-nemne/inferences-server/kubernetes-manifests/inference.deployment.yaml"))
-				.get();
-			result = aDeployment.toString();
-			logger.info("deployment spec: {}", aDeployment.getSpec());
-			Deployment createdDeployment = client.apps().deployments().inNamespace("default").createOrReplace(aDeployment);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		//create service
+		client.services()
+			.inNamespace("default")
+			.createOrReplace(service);
+		logger.info("service spec: {}", service.getSpec());
 
-		return result;
+		Map<String, Quantity> reqMap = new HashMap<>();
+		reqMap.put("cpu", new Quantity("1"));
+		reqMap.put("memory", new Quantity("1024Mi"));
+
+		ResourceRequirements resourceRequirements = new ResourceRequirementsBuilder()
+			.withLimits(reqMap).build();
+
+		Deployment deployment = new DeploymentBuilder()
+			.withNewMetadata()
+			.withName("inference-deployment")
+			.endMetadata()
+			.withNewSpec()
+			.withReplicas(2)
+			.withNewTemplate()
+			.withNewMetadata()
+			.addToLabels("app", "myinference")
+			.endMetadata()
+			.withNewSpec()
+			.addNewContainer()
+			.withName("bentoml-inference")
+			.withImage("dlatqdlatq/tf_service:latest")
+			.withResources(resourceRequirements)
+			.addNewPort()
+			.withContainerPort(5000)
+			.endPort()
+			.endContainer()
+			.endSpec()
+			.endTemplate()
+			.withNewSelector()
+			.addToMatchLabels("app", "myinference")
+			.endSelector()
+			.endSpec()
+			.build();
+
+		//create deployment
+		client.apps().deployments().inNamespace("default").createOrReplace(deployment);
+		logger.info("deployment spec: {}", deployment.getSpec());
+
+		return deployment.toString();
 	}
 }
